52f6e67ef1302dc2293301e0bcf18b61e2fb538c
Danil Kolumbet <kolumbetko@gmail.com>
2019-09-04 23:03:12 +0300

    add gradle

---
+++ src/main/java/tk/kolumb/PingMessage.java
@@ -0,0 +1,20 @@
+package tk.kolumb;
+
+public class PingMessage {
+    private String name;
+
+    public void Message() {
+    }
+
+    public void Message(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/tk/kolumb/Server.java
@@ -0,0 +1,5 @@
+package tk.kolumb;
+
+public class Server {
+
+}
